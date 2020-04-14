3ea8540abe7fd12b946313f27936e43f37ad5d8f
gy <357146094@qq.com>
2020-04-14 11:44:51 +0800

    init

---
+++ src/com/gy/Test.java
@@ -0,0 +1,26 @@
+package com.gy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    String s;
+
+    public Test() {
+        System.out.println();
+
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("ssss");
+        List list = new ArrayList<>();
+    }
+}
